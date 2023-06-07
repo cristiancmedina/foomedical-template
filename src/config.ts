f096351fda8d299ae03b9664585acc697f4e313f
Cristian Medina <cristianc@brightbots.co>
2023-06-06 20:15:03 -0500

    📦️ Update packages

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Default values work on localhost:3000 and foomedical.com
 // Replace these values with your own values for production
-export const MEDPLUM_PROJECT_ID = '9602358d-eeb0-4de8-bccf-e2438b5c9162';
-export const MEDPLUM_GOOGLE_CLIENT_ID = '679052511930-8dqur4mmg8egbttgos5pmr4ljtf3etbb.apps.googleusercontent.com';
+export const MEDPLUM_PROJECT_ID = '41b687c6-1b59-4ddc-885c-753cd2425d2a';
+export const MEDPLUM_GOOGLE_CLIENT_ID = '921088377005-3j1sa10vr6hj86jgmdfh2l53v3mp7lfi.apps.googleusercontent.com';
 export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LfFd_8gAAAAAOCVrZQ_aF2CN5b7s91NEYIu5GxL';
